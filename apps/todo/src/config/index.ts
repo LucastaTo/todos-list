c635b8837071fb1bf4a747675a76578c624decb9
LAPTOP-QL1FQ6F3\SPThe <spthehung.1802.22@gmail.com>
2024-06-03 23:18:23 +0700

    fix conffig

--- apps/todo/src/config/index.ts
+++ apps/todo/src/config/index.ts
@@ -1,3 +1,6 @@
-const apiUrl = import.meta.env.VITE_REACT_BASE_URL;
+const apiUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5174"
+    : import.meta.env.VITE_REACT_BASE_URL;
 
 export default apiUrl;
